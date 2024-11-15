362051aeae2cfdad0f2f993497f72a2dce68417f
ambzhang <ambzhang@tencent.com>
2024-11-15 10:31:05 +0800

    chore(script): 优化Linux下Cypress安装脚本

--- cypress/script/linux/install.sh
+++ cypress/script/linux/install.sh
@@ -18,9 +18,12 @@ pnpm install typescript
 
 npx tsc
 
-npx cypress install 
 
 apt-get update
 apt-get install -y xvfb
 apt-get install -y libnss3 libgconf-2-4 libxss1 libasound2 libatk1.0-0 libatk-bridge2.0-0 libcups2 libdrm2 libxcomposite1 libxdamage1 libxrandr2 libgbm1 libpango-1.0-0 libpangocairo-1.0-0
 apt-get install -y libgtk-3-0 libnss3 libgconf-2-4 libxss1 libasound2 libatk1.0-0 libatk-bridge2.0-0 libcups2 libdrm2 libxcomposite1 libxdamage1 libxrandr2 libgbm1 libpango-1.0-0 libpangocairo-1.0-0
+
+
+cd ${TESTSOLAR_WORKSPACE}
+npx cypress install 
